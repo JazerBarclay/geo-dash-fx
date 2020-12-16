9c61b458d7204b2a7a2f0f7d5b0e0deb32c70c1c
Jazer <contact@jazer.co.uk>
2020-12-16 15:08:00 +0000

    Added example JavaFX launcher code

--- geo-dash-fx/src/dev/jazer/java/geodashfx/Launcher.java
+++ geo-dash-fx/src/dev/jazer/java/geodashfx/Launcher.java
@@ -1,9 +1,26 @@
 package dev.jazer.java.geodashfx;
 
-public class Launcher {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 
-	public static void main(String[] args) {
-		System.out.println("Hello Team JAM!");
+
+public class Launcher extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
-
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
+
