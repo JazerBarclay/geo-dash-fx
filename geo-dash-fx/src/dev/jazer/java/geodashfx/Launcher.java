911f2f2e853adad233a514b735222350d9868a77
Jazer <contact@jazer.co.uk>
2020-12-16 15:15:09 +0000

    Added minimal code for fxgl game to run

--- geo-dash-fx/src/dev/jazer/java/geodashfx/Launcher.java
+++ geo-dash-fx/src/dev/jazer/java/geodashfx/Launcher.java
@@ -1,26 +1,19 @@
 package dev.jazer.java.geodashfx;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
 
 
-public class Launcher extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+public class Launcher extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(800);
+        settings.setHeight(600);
+        settings.setTitle("Basic Game App");
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
-
